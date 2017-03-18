5aca5072e35e3bae2b09c065eb12040a3a823504
Boris Barroso <boriscyber@gmail.com>
2017-03-18 19:44:20 -0400

    Better navigation layout

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,11 @@
 defmodule Publit.LayoutView do
   use Publit.Web, :view
+
+  def active_link(conn, uri) do
+    {:ok, reg} = Regex.compile(conn.request_path)
+
+    if Regex.match?(reg, uri) do
+      "active"
+    end
+  end
 end
