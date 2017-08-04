936b16cda88042d12431a39b2185ee1442841697
boriscy <boriscyber@gmail.com>
2017-08-04 06:54:20 -0400

    Updated version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Publit.Mixfile do
 
   def project do
     [app: :publit,
-     version: "0.9.13",
+     version: "0.9.14",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
